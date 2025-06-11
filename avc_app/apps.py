a9004b3b8e742476d8a47d79d528a5447debe5ed
zubbby <nwokenzube54@gmail.com>
2025-06-11 22:49:27 +0100

    Update apps.py

--- avc_app/apps.py
+++ avc_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AvcAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'avc_app'
+
+    def ready(self):
+        import avc_app.signals  # Import signals when app is ready
